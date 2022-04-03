4b38a4899a254e1bbe32b6986c715d7f7137d2e6
Olzx <ol2az@yandex.ru>
2022-04-04 01:49:24 +0700

    Added a class to highlight players in the world

---
+++ src/main/java/ru/lywi/world/render/PlayerRender.java
@@ -0,0 +1,27 @@
+package ru.lywi.world.render;
+
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderContext;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Vec3d;
+import ru.lywi.screen.PlayersList;
+import ru.lywi.world.render.utils.WorldRender;
+
+public class PlayerRender {
+    public static void playerBox(WorldRenderContext wrc) {
+        PlayerEntity player = MinecraftClient.getInstance().player;
+        if (player == null) return;
+
+        BlockPos playerPos = player.getBlockPos();
+        Vec3d cameraPos = wrc.gameRenderer().getCamera().getPos();
+
+        for (Entity entity: PlayersList.playersList) {
+            Box box = new Box(entity.getBlockPos()).offset(cameraPos.negate());
+            WorldRender.renderBox(box);
+            WorldRender.renderBox(box.offset(0, 1, 0));
+        }
+    }
+}
