a399c726b8b62ed2c94e7321436db9ec79a4eac0
Olzx <ol2az@yandex.ru>
2022-04-05 20:46:23 +0700

    Added a button to close the screen

--- src/main/java/ru/lywi/screen/PlayersList.java
+++ src/main/java/ru/lywi/screen/PlayersList.java
@@ -5,6 +5,7 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
 import ru.lywi.world.entity.EntityUtils;
 
@@ -22,6 +23,8 @@ public class PlayersList extends Screen {
     }
 
     public void init() {
+        this.addDrawableChild(new ButtonWidget(20, this.height-40, 50, 20, new LiteralText("Закрыть"), action -> this.client.setScreen((Screen)null)));
+
         int margin = 20;
         for (Entity entity : playersList) {
             this.addDrawableChild(new ButtonWidget(20, margin, 60, 20, entity.getDisplayName(), action -> {
