29743c0df1c90220bb47ff0f41cd6bcdc8065cc6
Olzx <ol2az@yandex.ru>
2022-04-10 23:16:45 +0700

    Added constructor and getters

--- src/main/java/ru/lywi/world/entity/EntityDistance.java
+++ src/main/java/ru/lywi/world/entity/EntityDistance.java
@@ -1,5 +1,21 @@
 package ru.lywi.world.entity;
 
-public class EntityDistance {
+import net.minecraft.entity.Entity;
 
+public class EntityDistance {
+    private final Entity entity;
+    private final int distance;
+
+    public EntityDistance(Entity entity, int distance) {
+        this.entity = entity;
+        this.distance = distance;
+    }
+
+    public Entity getEntity() {
+        return entity;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
 }
